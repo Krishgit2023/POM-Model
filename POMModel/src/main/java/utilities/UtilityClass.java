29f60632ce5ec3034705582fbd080d0349cd66b2
krishna <krishna@LAPTOP-O0RL6D68>
2023-04-18 14:13:54 +0530

    Commiting test change made

--- POMModel/src/main/java/utilities/UtilityClass.java
+++ POMModel/src/main/java/utilities/UtilityClass.java
@@ -5,8 +5,7 @@ import org.openqa.selenium.WebElement;
 public class UtilityClass {
 	
 	public String get_textofelement(WebElement element) {
-		return element.getText();
-		
+		return element.getText();	
 	}
 
 }
